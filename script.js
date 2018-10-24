bb5fdd4a91cb98b66ae9bac206cfb435727266b7
Nayem <nayem.alam@mail.mcgill.ca>
2018-10-24 15:11:26 -0400

    updated button and scripts

---
+++ script.js
@@ -0,0 +1,13 @@
+
+window.onload = function() {
+  // console.log("hello")
+
+
+// Adding an item
+document.getElementById('add').addEventListener('click', function(){
+  var val = document.getElementById('item').value
+  console.log(val)
+
+});
+
+}
